139d9ee18395d605d9fbbb3b7444331d21e7e3c7
JRPires10 <135224251+JRPires10@users.noreply.github.com>
2023-07-11 15:54:02 +0100

    Revert "Update streamlit_app.py"
    This reverts commit 218350792817c1e0399f920c48d953dfcb9ea726.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,14 +26,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display frutivyce api response
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+#Import requests to display API response
+#import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+#take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
